bb9263c955dd20ba60664dc00a80fa3d0fbcb022
burakkkara <burak.kara@ozu.edu.tr>
2019-11-17 16:16:08 +0300

    init backend and frontend.

---
+++ src/test/java/com/cs434project/Cs434projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs434project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs434projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
