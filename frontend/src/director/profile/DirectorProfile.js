78c46ae593506be06b78840d736154c825ad59bd
burakkkara <burak.kara@ozu.edu.tr>
2019-11-19 12:41:43 +0300

    init branch, added DirectorProfile file

---
+++ frontend/src/director/profile/DirectorProfile.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class DirectorProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            director: null
+        }
+    }
+}
